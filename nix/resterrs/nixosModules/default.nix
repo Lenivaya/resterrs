b9693555d294caf3f65e06dadcf15e131a514640
Lenivaya <danylo.osipchuk@proton.me>
2024-10-27 00:04:20 +0300

    feat: add package and nixosModule for nix

---
+++ nix/resterrs/nixosModules/default.nix
@@ -0,0 +1,54 @@
+perSystem:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.services.resterrs;
+
+  inherit (lib.meta) getExe';
+  inherit (lib.options) mkEnableOption mkPackageOption mkOption;
+
+  toTOML = pkgs.formats.toml { };
+in
+{
+  options.services.resterrs = {
+    enable = mkEnableOption "Resterrs, simple rester";
+    package = mkPackageOption pkgs "resterrs" { } // {
+      default = perSystem.config.packages.resterrs;
+    };
+
+    settings = mkOption {
+      type = toTOML.type;
+      default = { };
+      example = literalExpression ''
+        system_services_to_stop = ["bpftune", "syncthing", "fwupd"]
+        user_services_to_stop = ["kdeconnect", "picom"]
+        apps_to_stop = ["telegram-desktop", "vesktop"]
+        username = "leniviy"
+      '';
+      description = "TOML configuration for Resterrs. See the Resterrs documentation for available options.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.resterrs = {
+      description = "Resterrs Service";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        ExecStart = "${getExe' cfg.package "resterrs"} -c ${toTOML.generate "resterrs-config.toml" cfg.settings}";
+        Restart = "always";
+        RestartSec = "10";
+        User = "root";
+        DynamicUser = true;
+        StateDirectory = "resterrs";
+        ConfigurationDirectory = "resterrs";
+        RuntimeDirectory = "resterrs";
+      };
+    };
+  };
+}
